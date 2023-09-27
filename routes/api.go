b87ede658a885dcfc976d5767f38e50df9a5f926
tian <whales.fall.w@gmail.com>
2023-09-27 09:58:05 +0800

    通过手机找回密码

--- routes/api.go
+++ routes/api.go
@@ -39,6 +39,9 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/login/using-password", lgc.LoginByPassword)
 			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
 		
+			// 重置密码
+            pwc := new(auth.PasswordController)
+            authGroup.POST("/password-reset/using-phone", pwc.ResetByPhone)
 		}
 	}
 }
