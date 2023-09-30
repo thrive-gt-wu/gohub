04db48201d7cc22c7be6d6f701d761ddfedc0625
tian <whales.fall.w@gmail.com>
2023-09-30 16:19:42 +0800

    分类模型

---
+++ app/models/category/category_model.go
@@ -0,0 +1,31 @@
+// Package category 模型
+package category
+
+import (
+	"gohub/app/models"
+	"gohub/pkg/database"
+	_"gohub/pkg/logger"
+)
+
+type Category struct {
+    models.BaseModel
+
+    Name        string `json:"name,omitempty"`
+    Description string `json:"description,omitempty"`
+
+    models.CommonTimestampsField
+}
+
+func (category *Category) Create() {
+    database.DB.Create(&category)
+}
+
+func (category *Category) Save() (rowsAffected int64) {
+    result := database.DB.Save(&category)
+    return result.RowsAffected
+}
+
+func (category *Category) Delete() (rowsAffected int64) {
+    result := database.DB.Delete(&category)
+    return result.RowsAffected
+}
